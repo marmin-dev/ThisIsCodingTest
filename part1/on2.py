e9f012d06e1f5535222202793b6fe4a6dc7d250e
marmin-dev <marmin.dev@gmail.com>
2023-04-14 16:40:56 +0900

    part 1 end studied about complexity and timing code

---
+++ part1/on2.py
@@ -0,0 +1,6 @@
+array = [3,5,1,2,4] # (N = 5)
+
+for i in array:
+    for j in array:
+        temp = i * j
+        print(temp)
---
+++ part1/timing_code.py
@@ -0,0 +1,6 @@
+import time
+start_time = time.time()
+
+# 프로그램 소스코드
+end_time = time.time()
+print("time: ",end_time-start_time)
