5004c72aed3485ec56ddded63662bfa109fdb655
marmin-dev <marmin.dev@gmail.com>
2023-05-19 08:58:01 +0900

    morning coding test prac

--- part05/bfs_prac.py
+++ part05/bfs_prac.py
@@ -8,7 +8,7 @@ def bfs(graph, start, visited):
     while queue:
         v = queue.popleft()
         print(v, end=' ')
-        # 해당 언소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
