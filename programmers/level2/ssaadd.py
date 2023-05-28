eeba9ebfae78c913320832d7ba14230245c3bc7b
marmin-dev <marmin.dev@gmail.com>
2023-05-28 21:33:49 +0900

    short_que

--- programmers/level2/ssaadd.py
+++ programmers/level2/ssaadd.py
@@ -2,15 +2,22 @@ def solution(s):
     answer = 0
     li = []
     s = list(s)
-    while s:
+    aa = True
+    while aa:
         sa = s.pop()
+        if not s:
+            answer = 0
+            break
         if sa == s[-1]:
             s.pop()
-            s = s.extend(li)
+            s.extend(li)
             li = []
         else:
             li.append(sa)
-
+        if not s and not li:
+            answer = 1
+            aa = False
     return answer
 
-print(solution('baabaa'))
+# print(solution('baabaa'))
+print(solution('cdcd'))
